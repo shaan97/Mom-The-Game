d0cace454b5d3c4eb3978283ce3bdb57c673985d
Shaan Mathur <shaankaranmathur@gmail.com>
2016-12-13 22:56:43 -0800

    Convert
    Made modifications for the convert function, though it still needs to be
    tested.

--- Source/Mom_Regen/maze_globals.cpp
+++ Source/Mom_Regen/maze_globals.cpp
@@ -1,6 +1,7 @@
 #include "Mom_Regen.h"
 
 #include "maze_globals.h"
+#include <cassert>
 
 using namespace MazeAlg;
 
@@ -14,8 +15,8 @@ Location MazeAlg::convert(const FVector& pLocation)
 {
 	FVector temp = pLocation - START_VECTOR;
 
-	int r = (temp.Y / MazeAlg::MAZE_SIDE_LENGTH) + ((int)(temp.Y) % (int)(MazeAlg::MAZE_SIDE_LENGTH) >= MazeAlg::MAZE_SIDE_LENGTH / 2 ? 1 : 0);
-	int c = (1.0 / MazeAlg::MAZE_SIDE_LENGTH)*(temp.X) + ((int)(temp.X) % (int)(MazeAlg::MAZE_SIDE_LENGTH) >= MazeAlg::MAZE_SIDE_LENGTH / 2 ? 1 : 0);
-
-	return Location(-r, -c); // This might be incorrect
+	int c = (temp.Y / MazeAlg::PIECE_SIDE_LENGTH);// +((int)(temp.Y) % (int)(MazeAlg::PIECE_SIDE_LENGTH) >= MazeAlg::PIECE_SIDE_LENGTH / 2 ? 1 : 0);
+	int r = (temp.X / MazeAlg::PIECE_SIDE_LENGTH);// +((int)(temp.X) % (int)(MazeAlg::PIECE_SIDE_LENGTH) >= MazeAlg::PIECE_SIDE_LENGTH / 2 ? 1 : 0);
+	
+	return Location(r, c); // This might be incorrect
 }
